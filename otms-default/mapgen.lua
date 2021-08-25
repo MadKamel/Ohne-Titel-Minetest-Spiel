663dc602c8a33b13409dda2f173524060e3fca51
Eric Rustrum <rustrumek@gmail.com>
2021-08-25 13:04:52 -0700

    Added English comments.

--- otms-default/mapgen.lua
+++ otms-default/mapgen.lua
@@ -1,3 +1,4 @@
+-- register mapgen aliases for v7.
 -- regestrierer Mapgen-Aliasnamen für v7
 minetest.register_alias("mapgen_stone", "otms_default:stone")
 minetest.register_alias("mapgen_water_source", "otms_default:water_source")
