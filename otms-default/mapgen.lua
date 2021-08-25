3be6721e233266d067d18ea5f38c2686041c04bc
Eric Rustrum <rustrumek@gmail.com>
2021-08-25 12:26:34 -0700

    Ich schreibt mapgen.lua

---
+++ otms-default/mapgen.lua
@@ -0,0 +1,4 @@
+-- regestrierer Mapgen-Aliasnamen für v7
+minetest.register_alias("mapgen_stone", "otms_default:stone")
+minetest.register_alias("mapgen_water_source", "otms_default:water_source")
+minetest.register_alias("mapgen_river_water_source", "otms_default:river_water_source")
