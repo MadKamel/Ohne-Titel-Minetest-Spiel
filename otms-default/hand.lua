6a9b1d8a23136f58d11b7719340c0d60c63f8238
Eric Rustrum <rustrumek@gmail.com>
2021-08-25 12:33:35 -0700

    Added English comments.

--- otms-default/hand.lua
+++ otms-default/hand.lua
@@ -1,7 +1,7 @@
 minetest.register_item(':', {
 	type = 'none',
-	wield_image = 'Hand.png', -- Nicht hier jetzt, aber bald.
-	wield_scale = {x = 0.5, y = 1, z = 4}, -- Config von Void Spiel
+	wield_image = 'hand.png', -- Nicht hier jetzt, aber bald. (Not here yet, but soon.)
+	wield_scale = {x = 0.5, y = 1, z = 4}, -- Config von Void Spiel (Config from Void game)
 	tool_capabilities = {
 		full_punch_interval = 1.0,
 		max_drop_level = 0.0,
