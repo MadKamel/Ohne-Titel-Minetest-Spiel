b0d4e469d239a1b18791ae5622143062a099ab53
Eric Rustrum <rustrumek@gmail.com>
2021-09-02 14:25:22 -0700

    Tried overriding the day/night ratio.

--- mods/otms_default/init.lua
+++ mods/otms_default/init.lua
@@ -2,6 +2,12 @@
 -- Import locale.
 local S = minetest.get_translator("otms_default")
 
+
+
+minetest.override_day_night_ratio(1)
+
+
+
 -- Import subgame mods.
 local modpath = minetest.get_modpath("otms_default")
 
