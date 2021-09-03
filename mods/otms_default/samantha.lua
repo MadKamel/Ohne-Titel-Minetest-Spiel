7a1925474425c6159116d792895b478f531f8dc3
Eric Rustrum <rustrumek@gmail.com>
2021-09-03 15:40:20 -0700

    Added food sources table.

--- mods/otms_default/samantha.lua
+++ mods/otms_default/samantha.lua
@@ -1,7 +1,11 @@
 local persona = {
-    fear_threshold = 5, -- How cowardly Samantha is
-    panic_threshold = 5, -- How much fear is too much for her
-    boredom_tolerance = 5, -- How much entertainment Samantha wants
-    min_hunger_threshold = 5, -- How hungry until Samamtha thinks to eat
-    max_hunger_threshold = 10, -- How hungry Samantha can get before panic
+	fear_threshold = 5, -- How cowardly Samantha is
+	panic_threshold = 5, -- How much fear is too much for her
+	boredom_tolerance = 5, -- How much entertainment Samantha wants
+	min_hunger_threshold = 5, -- How hungry until Samamtha thinks to eat
+	max_hunger_threshold = 10, -- How hungry Samantha can get before panic
+}
+
+local food_sources = {
+	"otms_default:grass"
 }
