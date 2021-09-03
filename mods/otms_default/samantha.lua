c49de6206a300b81bc3a7d864985747e13d1f8f0
Eric Rustrum <rustrumek@gmail.com>
2021-09-03 15:35:52 -0700

    Added the persona for Samantha.

--- mods/otms_default/samantha.lua
+++ mods/otms_default/samantha.lua
@@ -1,1 +1,7 @@
-
+local persona = {
+    fear_threshold = 5, -- How cowardly Samantha is
+    panic_threshold = 5, -- How much fear is too much for her
+    boredom_tolerance = 5, -- How much entertainment Samantha wants
+    min_hunger_threshold = 5, -- How hungry until Samamtha thinks to eat
+    max_hunger_threshold = 10, -- How hungry Samantha can get before panic
+}
